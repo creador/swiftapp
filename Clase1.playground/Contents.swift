dfe224e12bf86e4d5c2068ee46c082dbff9510bc
Pablo Schaffner (imac27) <pablo@creador.cl>
2016-10-14 09:14:32 -0300

    Mi primer commit

---
+++ Clase1.playground/Contents.swift
@@ -0,0 +1,7 @@
+var birthYear = 1979
+
+if birthYear < 2000 {
+    print("naciste en el siglo 20")
+} else {
+    print("naciste en el siglo 21")
+}
---
+++ Swift App/Swift App/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Pablo Schaffner on 12-10-16.
+//  Copyright © 2016 Pablo Schaffner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mitexto: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func botonApretado(_ sender: AnyObject) {
+        tapCount = tapCount + 1
+        if tapCount >= 10 {
+            mitexto.text = "Apretaste 10 veces el boton!"
+        }
+    }
+    
+    @IBAction func boton2Apretado(_ sender: AnyObject) {
+        mitexto.text = "Esto es cool!"
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
